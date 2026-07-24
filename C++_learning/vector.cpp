b082df509c562e513f862037fec801389ee9257b
ZZZZ-D <3133846809@qq.com>
2026-07-24 22:20:14 +0800

    Add files via upload

---
+++ C++_learning/vector.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+// int main()
+// {
+//     vector<int> v1(2, 100);
+//     v1.push_back(20);
+//     v1.emplace_back(12);
+//     v1.erase(v1.begin());
+//     vector<int>::iterator iit = v1.begin(); // 迭代器
+//     for (iit = v1.begin(); iit != v1.end(); iit++)
+//     {
+//         cout << (*iit) << endl;
+//     }
+//     v1.insert(v1.begin(), 10); // 插入
+//     for (auto it : v1)
+//     {
+//         cout << it << endl;
+//     }
+//     v1.pop_back();
+//     return 0;
+// }
